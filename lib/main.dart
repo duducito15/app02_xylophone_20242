0432eb6b97550cddbbeae22a65eb250ce779a98e
Eduardo <echavezv@utea.edu.pe>
2024-12-26 06:58:29 -0500

    Xylophone completo

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,12 +17,35 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   final player = AudioPlayer();
 
+  void playNote(int note) {
+    player.setSourceAsset("audios/note$note.wav");
+    player.play(
+      AssetSource("audios/note$note.wav"),
+    );
+  }
+
+  Widget buildKey(Color colorKey, int note) {
+    return Expanded(
+      child: Container(
+        color: colorKey,
+        child: TextButton(
+          onPressed: () {
+            playNote(note);
+          },
+          child: const Text(""),
+        ),
+      ),
+    );
+  }
+
+  HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text(
+        title: const Text(
           "Xylophone",
           style: TextStyle(color: Colors.white),
         ),
@@ -29,29 +54,13 @@ class HomePage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            child: Container(
-              color: Colors.pink,
-              child: TextButton(
-                onPressed: () {
-                  player.setSourceAsset("audios/note1.wav");
-                  player.play(
-                    AssetSource("audios/note1.wav"),
-                  );
-                },
-                child: Text(""),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.indigo,
-              child: TextButton(
-                onPressed: () {},
-                child: Text(""),
-              ),
-            ),
-          ),
+          buildKey(Color(0xFF001219), 1),
+          buildKey(Color(0xFF005f73), 2),
+          buildKey(Color(0xFF0a9396), 3),
+          buildKey(Color(0xFF94d2bd), 4),
+          buildKey(Color(0xFFe9d8a6), 5),
+          buildKey(Color(0xFFee9b00), 6),
+          buildKey(Color(0xFFca6702), 7),
         ],
       ),
     );
